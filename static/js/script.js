206eec43fabc164e2500bfaeb2eed772b468c0c2
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-07-28 10:23:05 +0000

    init: finally created my portfolio website

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+// Add your JavaScript functionality here
+// For example, you can add an event listener to the connect wallet button
+
+document.querySelector('.connect-wallet').addEventListener('click', () => {
+    alert('Connect Wallet button clicked!');
+});
